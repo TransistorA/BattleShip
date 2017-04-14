73a64150aa9a665cfb03dd7573ead1237fb7639d
Joseph DiPalma <jrd028@bucknell.edu>
2017-04-14 08:30:10 -0400

    Updated Model and Controller

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -25,9 +25,71 @@ import javafx.event.EventHandler;
  */
 public class Controller implements EventHandler<ActionEvent> {
 
+    private Model theModel;
+    private View theView;
+
+    /**
+     * Constructor for the Controller class.
+     *
+     * @author Joseph DiPalma
+     *
+     * @param theModel model part of the MVC design pattern
+     * @param theView view part of the MVC design pattern
+     */
+    public Controller(Model theModel, View theView) {
+        this.theModel = theModel;
+        this.theView = theView;
+
+        // This is all things that haven't been put in View yet but will be
+        // in the future.
+        /*
+        this.theView.getRotateCWButton().setOnAction(this);
+        this.theView.getRotateCCWButton().setOnAction(this);
+
+        this.theModel.getCarrier().bind(this.theView.getCarrierBtn().selectedProperty());
+        this.theModel.getBattleship().bind(this.theView.getBattleshipBtn().selectedProperty());
+        this.theModel.getCruiser().bind(this.theView.getCruiserBtn().selectedProperty());
+        this.theModel.getSubmarine().bind(this.theView.getSubmarineBtn().selectedProperty());
+        this.theModel.getDestroyer().bind(this.theView.getDestroyerBtn().selectedProperty());
+         */
+    }
+
+    /**
+     * Event handler for the GUI.
+     *
+     * @author Joseph DiPalma
+     *
+     * @param event event from the GUI to respond to
+     *
+     * This code was based off of information found at the below website.
+     *
+     * @see
+     * <a href="http://stackoverflow.com/questions/25409044/javafx-multiple-buttons-to-same-handler">
+     * http://stackoverflow.com/questions/25409044/javafx-multiple-buttons-to-same-handler</a>
+     */
     @Override
     public void handle(ActionEvent event) {
 
+        // This is all things that haven't been put in View yet but will be
+        // in the future.
+        /*
+        try {
+            if (event.getSource() == rotateCWButton) {
+                // Call the rotateCW method in Ship.
+            }
+
+            else if (event.getSource() == rotateCCWButton) {
+                // Call the rotateCCW method in Ship.
+            }
+        }
+        catch (RotationException ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Rotation failed!");
+            alert.setHeaderText("Rotation failed!");
+            alert.setContentText("Cannot rotate the specified ship!");
+            alert.show();
+        }
+         */
     }
 
 }
--- src/GUI/Model.java
+++ src/GUI/Model.java
@@ -15,11 +15,53 @@
  */
 package GUI;
 
+import javafx.beans.property.SimpleBooleanProperty;
+
 /**
  * Model part of the MVC design pattern.
  *
  * @author Joseph DiPalma
  */
 public class Model {
+
+    // Figure out which Ship the user wants to add.
+    private SimpleBooleanProperty carrier;
+    private SimpleBooleanProperty battleship;
+    private SimpleBooleanProperty cruiser;
+    private SimpleBooleanProperty submarine;
+    private SimpleBooleanProperty destroyer;
+
+    /**
+     * Constructor for the Model class.
+     *
+     * @author Joseph DiPalma
+     */
+    public Model() {
+        this.carrier = new SimpleBooleanProperty(true);
+        this.battleship = new SimpleBooleanProperty(false);
+        this.cruiser = new SimpleBooleanProperty(false);
+        this.submarine = new SimpleBooleanProperty(false);
+        this.destroyer = new SimpleBooleanProperty(false);
+    }
+
+    public SimpleBooleanProperty getCarrier() {
+        return carrier;
+    }
+
+    public SimpleBooleanProperty getBattleship() {
+        return battleship;
+    }
+
+    public SimpleBooleanProperty getCruiser() {
+        return cruiser;
+    }
+
+    public SimpleBooleanProperty getSubmarine() {
+        return submarine;
+    }
+
+    public SimpleBooleanProperty getDestroyer() {
+        return destroyer;
+    }
 
 }
