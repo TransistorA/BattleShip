6134a7a6b6b6aed1431dfb82290da3f444202c69
Annan Miao <am049@bucknell.edu>
2017-04-30 23:23:55 -0400

    finish man vs computer

---
+++ src/GUI/haha.java
@@ -0,0 +1,54 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Spring 2017
+*
+* Name: Joseph DiPalma, Annan Miao, and Ben Xu
+* Date: Apr 30, 2017
+* Time: 11:14:30 PM
+*
+* Project: csci205finalproject
+* Package: GUI
+* File: haha
+* Description:
+*
+* ****************************************
+ */
+package GUI;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+/**
+ *
+ * @author am049
+ */
+public class haha extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws FileNotFoundException, IOException {
+        InputStream in = new FileInputStream(
+                "/nfs/unixspace/linux/accounts/student/a/am049/csci205FinalProject/src/furrow.mp3");
+        AudioStream as = new AudioStream(in);
+        AudioPlayer.player.start(as);
+        /*final URL resource = getClass().getResource(
+                "/nfs/unixspace/linux/accounts/student/a/am049/csci205FinalProject/src/furrow.mp3");
+        final Media media = new Media(resource.toString());
+        final MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+
+        primaryStage.setTitle("Audio Player 1");
+        primaryStage.setWidth(200);
+        primaryStage.setHeight(200);
+        primaryStage.show();*/
+    }
+}
